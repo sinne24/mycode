64999390bbccf771d40cc30eb1ffa949d2a52142
sinne24 <cindy.hernandez5224@gmail.com>
2023-01-04 22:01:12 +0000

    mixed lists

---
+++ lab18.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# below is a function containing our code
+def main():
+
+    # pause the program and wait for the user to provide input
+    user_name = input("Please enter your name:")
+
+    # pause the program and wait for the user to provide input
+    user_day = input("Please enter the day of the week:")
+
+    # display the input back to the user.
+    print("Hello, " + user_name + "! Happy "+ user_day +"!")
+
+# this calls main
+main()
